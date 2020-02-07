30a251c993f87173a648db08e806eae083ec70a0
Ody <Ody@Ody-PC>
2020-02-07 11:18:04 +0200

    0.2.4

--- katanemhmena/src/gr/hua/dit/service/AdminService.java
+++ katanemhmena/src/gr/hua/dit/service/AdminService.java
@@ -1,5 +1,9 @@
 package gr.hua.dit.service;
 
+import gr.hua.dit.entity.Student;
+
 public interface AdminService {
+
+	void addStudent(Student student);
 
 }
--- katanemhmena/src/gr/hua/dit/service/AdminServiceImpl.java
+++ katanemhmena/src/gr/hua/dit/service/AdminServiceImpl.java
@@ -1,5 +1,34 @@
 package gr.hua.dit.service;
 
-public class AdminServiceImpl implements AdminService {
+import javax.transaction.Transactional;
 
+import org.hibernate.Session;
+
+import gr.hua.dit.dao.DepartementDAO;
+import gr.hua.dit.dao.StudentDAO;
+import gr.hua.dit.entity.Departement;
+import gr.hua.dit.entity.Student;
+
+public class AdminServiceImpl implements AdminService {
+	
+	private DepartementDAO departementDAO;
+	private StudentDAO studentDAO;
+	@Override
+	@Transactional
+	public void addStudent(Student student) {
+		
+
+		// session.beginTransaction();
+
+		Departement dep = departementDAO.getDepartement(student.getDep().getDep_id());
+
+		dep.addStudent(student);
+
+		studentDAO.saveStudent(student);
+
+		// session.getTransaction().commit();
+
+		// System.out.println("Done!");
+
+	}
 }
